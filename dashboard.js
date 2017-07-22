4471daf69e1d14969ef85acaf0b7c2b6d1ca8f8f
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-07-22 15:02:10 -0400

    featured minimal api data, calendar, to-do list

--- dashboard.js
+++ dashboard.js
@@ -1,15 +1,15 @@
-function logOut(){
-    window.location.href="login.html";
-}
-
-$("#logOut").on('click', logOut);
-
 const DICE_URL = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?"
 
-//getJSON
-function getDiceApi(searchTerm, callback) {
+
+function getDiceApi(searchTerm, searchLocation, callback) {
     let params = {
-        q1: searchTerm,
+        "text": searchTerm,
+        "city": searchLocation,
+        "direct": 1,
+        "pgcnt": 10,
+        "skill": searchTerm,
+        "sort": "sort=4",
+        "sd": "sd=a",
     	type:"get",
         maxResults: 25
     }
@@ -17,21 +17,66 @@ function getDiceApi(searchTerm, callback) {
 }
 
 function displayResults(simple) {
+	$('#results').empty();
     let items = simple.resultItemList;
-    console.log(items);
-    console.log(items[0].location);
-    console.log(items[1].detailUrl);
+    for (let i=0; i<items.length; i++){
+   $('#results').append(`
+   		<div class="result-entry">
+   		<div id="title"><a href="${items[i].detailUrl}" target="blank">${items[i].jobTitle}</a></div>
+  		<div id="location">${items[i].location}</div>
+  		<div id="company">${items[i].company}</div>
+ 		<div id="date">${items[i].date}</div>
+ 		</div>
+ 	`)
     }
+}
+function taskHandler(event){
+	event.preventDefault();
+	let newTask = $("#entry").val();
+	$('.list').append(
+		`<li>
+          <span class="task">${newTask}</span><button class="check">check</button><button class="delete">delete</button>      
+        </li>
+      `);
+}
+function deleteHandler(event){
+	$('.delete').closest('li').remove();
+}
 
+function checkHandler(event) {
+	$('.check').closest('li').find('.task').toggleClass('.task_checked');
+}
+
+function logOut(){
+    window.location.href="login.html";
+}
+
+$("#logOut").on('click', logOut);
 
 $(document).ready(function (){
     $('#searchForm').submit(function(event) {
         event.preventDefault();
-        // get the search term and remove unnecessary white spaces
         let searchTerm= $(this).find('input[id="searchTerm"]').val().trim();
-        console.log(searchTerm);
         let searchLocation =$(this).find('input[id="searchLocation"]').val().trim();
-        console.log(searchLocation);
-        getDiceApi(searchTerm,displayResults)
+        getDiceApi(searchTerm, searchLocation, displayResults);
+    });
+
+	$('#toDo').submit(taskHandler);
+	$('.check').on('click',checkHandler);
+	$('.delete').on('click', deleteHandler);
+	$('#calendar').fullCalendar({
+        // put your options and callbacks here
+		schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+    	defaultView: 'agendaDay',
+    	events: [
+    	{
+            title: 'Event1',
+            start: '2017-07-22'
+        },
+        {
+            title: 'Event2',
+            start: '2017-07-25'
+        }
+    	]
     });
 });
