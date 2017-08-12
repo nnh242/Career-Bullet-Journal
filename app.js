d3a5ed2d3c2480b17a805f1413371c715c92a874
nnh242 <nhoang@me.com>
2017-08-12 15:36:21 -0400

    fixed all functional bugs

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 const DICE_URL = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?"
 
+
+$(document).ready(function (){
+    let rawDate = new Date();
+    let stringDate = rawDate.toString();
+    let displayDate= stringDate.slice(-57,-42);
+    $("#today").text(displayDate);
+    
+    // listen for user's search input
+    $('#searchForm').submit(function(event) {
+        event.preventDefault();
+        let searchTerm= $(this).find('input[id="searchTerm"]').val().trim();
+        let searchLocation =$(this).find('input[id="searchLocation"]').val().trim();
+        $('#results').removeClass('hidden');
+        getDiceApi(searchTerm, searchLocation, displayResults);
+    });
+	$('#toDoForm').submit(taskHandler);
+    $('#listForm').submit(listHandler);
+	$('.list').on('click','.check', checkHandler);
+	$('.list').on('click','.delete',deleteHandler);
+    $('.any-list').on('click','.delete',deleteHandler);
+
+//implement the weekly calendar 
+$('#calendar').fullCalendar({
+    schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+    defaultView: 'agendaDay',
+    selectable: true,
+    editable: true,
+    select: function(start, end, allDay) {
+    //popup modal for user to give event a name
+    $('#createEventModal').removeClass('hidden');
+    $('#eventName').val('');
+    $('#apptStartTime').val(start);
+    $('#apptEndTime').val(end);
+    $('#apptAllDay').val(allDay);
+}
+});
+});
+
 // get the data from Dice API
 function getDiceApi(searchTerm, searchLocation, callback) {
     let params = {
@@ -26,7 +64,7 @@ function displayResults(simple) {
 	   	<div class="result-entry">
 	   		<div id="title"><a href="${items[i].detailUrl}" target="blank">${items[i].jobTitle}</a></div>
 	  		<div id="location">${items[i].location}</div>
-	  		<div id="company">${items[i].company}</div>
+            <div id="company">${items[i].company}</div>  
 	 		<div id="date">${items[i].date}</div>
 	 	</div>
  	</div>
@@ -71,55 +109,27 @@ function logOut(){
     window.location.href="index.html";
 }
 $("#logOut").on('click', logOut);
-
+    
 // When user clicks on (x) close the modal
 $('.close').on('click', function () {
     $('#createEventModal').addClass('hidden');
 });
 
-$(document).ready(function (){
-    let rawDate = new Date();
-    let stringDate = rawDate.toString();
-    let displayDate= stringDate.slice(-57,-42);
-    $("#today").text(displayDate);
-    
-    // listen for user's search input
-    $('#searchForm').submit(function(event) {
-        event.preventDefault();
-        let searchTerm= $(this).find('input[id="searchTerm"]').val().trim();
-        let searchLocation =$(this).find('input[id="searchLocation"]').val().trim();
-        $('#results').removeClass('hidden');
-        getDiceApi(searchTerm, searchLocation, displayResults);
-    });
-	$('#toDoForm').submit(taskHandler);
-    $('#listForm').submit(listHandler);
-	$('.list').on('click','.check', checkHandler);
-	$('.list').on('click','.delete',deleteHandler);
-    $('.any-list').on('click','.delete',deleteHandler);
+$('#submitButton').on('click', function(e){
+    e.preventDefault();
+    console.log('event created');
+    $('#createEventModal').addClass('hidden');
 
-//implement the weekly calendar 
-$('#calendar').fullCalendar({
-        schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
-    	defaultView: 'agendaDay',
-        selectable: true,
-        editable: true,
-        select: function(start, end, allDay) {
-        //popup modal for user to give event a name
-        $('#createEventModal').removeClass('hidden');
-        $('#eventName').val('');
-        //submit event name
-        $('#createEvent').on('submit', function(e){
-        e.preventDefault();
-        $('#createEventModal').addClass('hidden');
-        $("#calendar").fullCalendar('renderEvent',
-            {
-            title: $('#eventName').val(),
-            start: new Date(start).toISOString(),
-            end: new Date(end).toISOString(),
-            allDay: false
-        },
-        true);
-        });  
-   }
-});
-});
+    console.log($('#apptStartTime').val());
+    console.log($('#apptEndTime').val());
+    console.log($('#apptAllDay').val());
+
+    $("#calendar").fullCalendar('renderEvent',
+        {
+        title: $('#eventName').val(),
+        start: (new Date($('#apptStartTime').val())).toISOString(),
+        end:  (new Date($('#apptEndTime').val())).toISOString(),
+        allDay: ($('#apptAllDay').val() == "true"),
+    },
+    true);
+    });  
