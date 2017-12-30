e570e8d3116192500b6c50fd4363b90adb6b5e90
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-12-30 12:17:27 -0500

    server

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const app = express();
 
 app.use(morgan('common'));
 
+//CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const https = require("https");
+const url =
+  "https://jobs.github.com/positions.json?";
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+  res.on("end", () => {
+    body = JSON.parse(body);
+  });
+});
+
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-  });
+});
+module.exports = {app};
