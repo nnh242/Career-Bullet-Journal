9cf3bcab2876f4f91636127f7b14fa0eb77bb09b
nnh242 <n.hoang@me.com>
2017-12-31 16:03:36 -0500

    added axios to get data

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-const https = require("https");
+//get the remote API and serve to front-end
 const url =
   "https://jobs.github.com/positions.json?";
-https.get(url, res => {
-  res.setEncoding("utf8");
-  let body = "";
-  res.on("data", data => {
-    body += data;
-  });
-  res.on("end", () => {
-    body = JSON.parse(body);
-  });
-});
 
+app.get(url,(req,res)=>{
+  axios
+  .get(url)
+  .then(res => {
+    console.log(res);
+    );
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
